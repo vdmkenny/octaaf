bc194689eb33b3d32dbbb8b25db5a90c28e331fc
Bart Willems <bwillems@protonmail.com>
2018-03-22 12:59:45 +0100

    Added REPORTED logging, removed most of the logic from main.go
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-
 	"github.com/gobuffalo/envy"
 	"gopkg.in/telegram-bot-api.v4"
 )
 
-var kaliCount int
+// KaliCount is an integer that holds the ID of the last send message in the Kali group
+var KaliCount int
+
+// KaliID is the ID of the kali group
+var KaliID int64
+
+// ReporterID is the id of the user who reports everyone
+var ReporterID int
 
 func main() {
 	envy.Load("config/.env")
@@ -20,8 +22,6 @@ func main() {
 	initBot()
 
 	go initCrons()
-
-	kaliID, _ := strconv.ParseInt(os.Getenv("TELEGRAM_ROOM_ID"), 10, 64)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -34,45 +34,6 @@ func main() {
 			continue
 		}
 
-		if update.Message.Chat.ID == kaliID {
-			log.Printf("Got message!")
-			kaliCount = update.Message.MessageID
-		}
-
-		if update.Message.IsCommand() {
-			switch update.Message.Command() {
-			case "roll":
-				go sendRoll(update.Message)
-			case "m8ball":
-				go m8Ball(update.Message)
-			case "bodegem":
-				go sendBodegem(update.Message)
-			case "img", "img_sfw":
-				go sendImage(update.Message)
-			case "stallman":
-				go sendStallman(update.Message)
-			case "avatar":
-				go sendAvatar(update.Message)
-			case "search", "search_nsfw":
-				go search(update.Message)
-			case "where":
-				go where(update.Message)
-			case "count":
-				go count(update.Message)
-			case "weather":
-				go weather(update.Message)
-			case "what":
-				go what(update.Message)
-			case "bol":
-				go bol(update.Message)
-			}
-		}
-
-		if update.Message.MessageID%100000 == 0 {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("💯💯💯💯 YOU HAVE MESSAGE %v 💯💯💯💯", update.Message.MessageID))
-			msg.ReplyToMessageID = update.Message.MessageID
-			msg.ParseMode = "markdown"
-			Octaaf.Send(msg)
-		}
+		go handle(update.Message)
 	}
 }
